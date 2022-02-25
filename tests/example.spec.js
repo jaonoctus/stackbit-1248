39a78d860a9cb4de3f860622de7e0649676c6c7c
jaonoctus <jaonoctus@protonmail.com>
2022-02-25 19:04:28 -0300

    chore: remove example test

--- tests/example.spec.js
+++
@@ -1,8 +0,0 @@
-const sum = (a = 0, b = 0) => parseInt(a, 10) + parseInt(b, 10)
-
-describe('Example test', () => {
-  it('Should sum two numbers', () => {
-    expect(sum(1, 2)).toBe(3)
-    expect(sum('1', '2')).toBe(3)
-  })
-})
